bbf4fc87ce8296cb8834901a99bfb75e60bb5751
Sergei <example@gmail.com>
2022-05-04 21:59:50 +0400

    first consol program 1.0

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-// ++incr;
-// --decr;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(incr++);
-console.log(decr--);
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
